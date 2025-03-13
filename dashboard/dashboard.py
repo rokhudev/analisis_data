47f917c0013512ce01de5e6521ca975c7868ba04
Muhamad Rokhul Affan <115555337+rokhudev@users.noreply.github.com>
2025-03-13 16:16:44 +0700

    Update dashboard.py
    update sidebar fitur trend pengiriman

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,19 +1,35 @@
 import pandas as pd
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
 sns.set(style='dark')
 
-# Load data
+# buka data
 all_data = pd.read_csv("main_datas.csv")
 
-# Streamlit app
+# Streamlit 
 st.title('Dashboard')
 
 # Sidebar
 st.sidebar.title('Sidebar')
 st.sidebar.image("foto-ecommerce.jpg", use_column_width=True)
+
+# Filter Trend Pengiriman
+st.sidebar.subheader("Filter Penyaringan Trend Pengiriman")
+all_data["order_delivered_customer_date"] = pd.to_datetime(all_data["order_delivered_customer_date"], errors='coerce')
+
+# Menentukan rentang tanggal 
+min_date = all_data["order_delivered_customer_date"].min()
+max_date = all_data["order_delivered_customer_date"].max()
+
+start_date, end_date = st.sidebar.date_input(
+    "Pilih rentang waktu:", 
+    [min_date, max_date], 
+    min_value=min_date, 
+    max_value=max_date
+)
 
 # Penyebaran Pelanggan
 st.subheader('Penyebaran Pelanggan')
@@ -24,12 +40,11 @@ ax_penyebaran_pelanggan.barh(customer_state.index,
            customer_state.values, 
            color="cyan")
 ax_penyebaran_pelanggan.set_title("Top 20 Kota Pelanggan", 
-                fontsize=30)
+                                  fontsize=30)
 ax_penyebaran_pelanggan.set_xlabel("Jumlah Pelanggan", 
-                 fontsize=16)
+                                   fontsize=16)
 ax_penyebaran_pelanggan.set_ylabel("Kota", 
-                 fontsize=16)
-
+                                   fontsize=16)
 st.pyplot(fig_penyebaran_pelanggan)
 
 # Distribusi Pengiriman
@@ -39,8 +54,8 @@ jumlah_pesanan_status = all_data["order_status"].value_counts()
 fig_distribusi_pelanggan, ax_distribusi_pelanggan = plt.subplots(figsize=(20, 10))
 sns.barplot(x=jumlah_pesanan_status.index, 
             y=jumlah_pesanan_status.values, 
-            palette="magma", ax=ax_distribusi_pelanggan)
-
+            palette="magma", 
+            ax=ax_distribusi_pelanggan)
 ax_distribusi_pelanggan.set_title("Status Distribusi Pengiriman Pesanan", 
                                   fontsize=30)
 ax_distribusi_pelanggan.set_ylabel("Jumlah Pesanan", 
@@ -56,14 +71,13 @@ for i, jumlah in enumerate(jumlah_pesanan_status.values):
 
 st.pyplot(fig_distribusi_pelanggan)
 
-# Trend Pengiriman
+# Trend Pengiriman 
 st.subheader('Trend Pengiriman')
+filtered_data = all_data[(all_data["order_delivered_customer_date"] >= pd.Timestamp(start_date)) & 
+                         (all_data["order_delivered_customer_date"] <= pd.Timestamp(end_date))]
 
-if not pd.api.types.is_datetime64_any_dtype(all_data["order_delivered_customer_date"]):
-    all_data["order_delivered_customer_date"] = pd.to_datetime(all_data["order_delivered_customer_date"])
-
-all_data["delivery_month_year"] = all_data["order_delivered_customer_date"].dt.to_period("M")
-trend_pengiriman = all_data.groupby("delivery_month_year").size()
+filtered_data["delivery_month_year"] = filtered_data["order_delivered_customer_date"].dt.to_period("M")
+trend_pengiriman = filtered_data.groupby("delivery_month_year").size()
 
 fig_trend_pengiriman, ax_trend_pengiriman = plt.subplots(figsize=(12, 6))
 sns.lineplot(x=trend_pengiriman.index.astype(str), 
@@ -71,7 +85,6 @@ sns.lineplot(x=trend_pengiriman.index.astype(str),
              marker="s", 
              color="green", 
              ax=ax_trend_pengiriman)
-
 ax_trend_pengiriman.set_title("Tren Bulan pada Pengiriman Pesanan")
 ax_trend_pengiriman.set_xlabel("Bulan dan Tahun")
 ax_trend_pengiriman.set_ylabel("Jumlah Pesanan Terkirim")
@@ -79,7 +92,6 @@ ax_trend_pengiriman.set_xticklabels(trend_pengiriman.index.astype(str), rotation
 ax_trend_pengiriman.grid(True)
 
 st.pyplot(fig_trend_pengiriman)
-
 
 # Clusterisasi kategori customers unique per kota
 st.subheader('Clusterisasi Kategori Customer Unique di setiap kota')
